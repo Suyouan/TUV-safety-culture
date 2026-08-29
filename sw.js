63d05bf06fe414f4d4fab5fe58cc6266cc1f8a7f
Suyouan <107753360+Suyouan@users.noreply.github.com>
2026-08-29 11:47:09 +0800

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
-const CACHE_NAME = "sign-sys-v1";
-const assetsToCache = [
-  "./index.html",
-  "./app.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('pwa-cache-v1').then((cache) => {
+            return cache.addAll(['./index.html', './app.js']);
+        })
+    );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
